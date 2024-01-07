4aac10a34e4a2451ae979b3fa497339d2148b537
IlhanYapici <ilhan.yapici@outlook.fr>
2024-01-07 14:07:06 +0100

    fix: sort exports ascending order

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
+export { ContactsLayout } from './ContactsLayout/ContactsLayout'
+export { IconButton } from './IconButton/IconButton'
 export { Login } from './Login/Login'
 export { MainLayout } from './MainLayout/MainLayout'
-export { IconButton } from './IconButton/IconButton'
-export { ContactsLayout } from './ContactsLayout/ContactsLayout'
 export { EventForm } from './events/EventForm/EventForm'
