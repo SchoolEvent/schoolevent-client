a0360d35dfad9b7d3d141a8f613c28bfef3555a6
IlhanYapici <ilhan-yapici@outlook.fr>
2024-02-05 18:28:04 +0100

    fix: event form fields type

--- src/components/events/EventForm/EventForm-types.ts
+++ src/components/events/EventForm/EventForm-types.ts
@@ -1,3 +1,12 @@
+import { Database } from '@types'
+
+type TEvent = Database['public']['Tables']['events']['Row']
+
+export interface IEventFormFields
+	extends Omit<TEvent, 'id' | 'event_participant_id' | 'event_creator_id' | 'event_background'> {
+	event_background?: { file: File; fileList: FileList }
+}
+
 export type TEventTypeLabel = 'Portes ouvertes' | 'Présentation' | 'Conférence'
 export type TEventTypeValue = 'portes_ouvertes' | 'presentation' | 'conference'
 
